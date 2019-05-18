9f96ca43f70b8073c1a4f5449c71894f4f3b84eb
Junaid Rasheed <junaid.rasheed@elisa.fi>
2019-05-18 04:39:38 +0300

    storybook support

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,10 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src', true, /\.stories\.js$/)
+// Get all stories from src
+const req = require.context('../src', true, /\.stories\.js$/);
 
 function loadStories() {
-
-  req.keys().forEach((filename) => req(filename))
-  //require('../stories/index.js');
-  // You can require as many stories as you need.
+  req.keys().forEach((filename) => req(filename));
 }
 
 configure(loadStories, module);
--- src/components/checkbox/Checkbox.stories.js
+++ src/components/checkbox/Checkbox.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import Checkbox from './';
 
 storiesOf('Button', module)
   .add('with text', () => (
